db0c308a0323512dd1473675592d74ddccc0fa8c
Karan Kumar k <karankabinesh@gmail.com>
2021-07-21 11:58:47 +0530

    first commit v2

---
+++ build/precache-manifest.bec9d070d39623b870ab26164f38b8da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5163b7c0944ce7afc39294f6890a875",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bd8556420fe572cbf45",
+    "url": "/static/css/main.0bbeb6f4.chunk.css"
+  },
+  {
+    "revision": "dd9c2236a01657008401",
+    "url": "/static/js/2.7ce5c32d.chunk.js"
+  },
+  {
+    "revision": "00e2b2cdc2475b76a408969f1af93df0",
+    "url": "/static/js/2.7ce5c32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd8556420fe572cbf45",
+    "url": "/static/js/main.f7dbad4d.chunk.js"
+  },
+  {
+    "revision": "7308dd93756735fe9a6a",
+    "url": "/static/js/runtime-main.ef316f09.js"
+  }
+]);
